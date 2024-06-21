6f0ae0c031c3af11e975db37fef7f0f1bececd0f
zbytes2227 <zbytes0@gmail.com>
2024-06-21 13:19:00 +0530

    pdf sys

--- app/admin/products/page.js
+++ app/admin/products/page.js
@@ -161,30 +161,30 @@ const Page = () => {
           </div>
 
           <div className="mt-5">
-          {Products && Array.isArray(Products) && Products.map((product)=> (
-            <div key={product._id} className="product">
+            {Products && Array.isArray(Products) && Products.map((product) => (
+              <div key={product._id} className="product">
 
-              <div class="max-w md:mx-10 p-6 bg-white flex justify-between items-center border border-gray-200 rounded-lg shadow mt-2">
-                <div>
-                  <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{product.ProductName}</h5>
-                  </a>
-                  <p class="mb-3 font-normal text-gray-700 ">{product.ProductBranch},Semester:{product.ProductSemester}  </p>
-                </div>
-                <div>
-                  <a href={product.ProductLink} class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
-                    Download
-                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                    </svg>
-                  </a>
+                <div class="max-w md:mx-10 p-6 bg-white flex justify-between items-center border border-gray-200 rounded-lg shadow mt-2">
+                  <div>
+                    <a href="#">
+                      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{product.ProductName}</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 ">{product.ProductBranch} | Semester- {product.ProductSemester}  | Year- {product.ProductSemester === "1" || product.ProductSemester === "2" ? 1 : product.ProductSemester === "3" || product.ProductSemester === "4" ? 2 : product.ProductSemester === "5" || product.ProductSemester === "6" ? 3 : ''} </p>
+                  </div>
+                  <div>
+                    <a href={`/admin/products/preview?id=${product.ProductLink}`} class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                      View
+                      <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                      </svg>
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
 
 
-))}
-</div>
+            ))}
+          </div>
 
           {!Loading ? "" : (
             <div role="status" className="flex justify-center">
---
+++ app/admin/products/preview/page.js
@@ -0,0 +1,54 @@
+
+"use client"
+import { useSearchParams } from 'next/navigation';
+import React, { useState } from 'react'
+
+const Page = () => {
+
+    const searchParams = useSearchParams();
+
+    const search = searchParams.get('id')
+    const [PDFid, setPDFid] = useState(search);
+
+    return (
+        <>
+            <div className='mt-20'>
+                <div className='flex justify-around'>
+
+
+<div class="inline-flex rounded-md shadow-sm" role="group">
+  <a href='/admin/products' type="button" class="inline-flex items-center px-4 py-2 text-sm font-medium text-blue-900 bg-transparent border border-blue-900 rounded-s-lg hover:bg-blue-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-blue-500 focus:bg-blue-900 focus:text-white ">
+  <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2"/>
+    </svg>
+    Back
+  </a>
+  <a href={`https://drive.usercontent.google.com/u/0/uc?id=${PDFid}&export=download>Download`} type="button" class="inline-flex items-center px-4 py-2 text-sm font-medium text-blue-900 bg-transparent border-t border-b border-blue-900 hover:bg-blue-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-blue-500 focus:bg-blue-900 focus:text-white ">
+  <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+      <path d="M14.707 7.793a1 1 0 0 0-1.414 0L11 10.086V1.5a1 1 0 0 0-2 0v8.586L6.707 7.793a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.416 0l4-4a1 1 0 0 0-.002-1.414Z"/>
+      <path d="M18 12h-2.55l-2.975 2.975a3.5 3.5 0 0 1-4.95 0L4.55 12H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2Zm-3 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"/>
+    </svg>
+    Download
+  </a>
+  <button type="button" class="inline-flex items-center px-4 py-2 text-sm font-medium text-blue-900 bg-transparent border border-blue-900 rounded-e-lg hover:bg-blue-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-blue-500 focus:bg-blue-900 focus:text-white ">
+  <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+      <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
+    </svg>
+    Share
+  </button>
+</div>
+
+                {/* <a >Back</a>
+                <a href={`https://drive.usercontent.google.com/u/0/uc?id=${PDFid}&export=download>Download`}>Download</a>
+                <a href={`https://drive.usercontent.google.com/u/0/uc?id=${PDFid}&export=download>Download`}>Share</a> */}
+                </div>
+                <div className='flex justify-center my-4'>
+                <iframe src={`https://drive.google.com/file/d/${PDFid}/preview`} width="710" height="900" allow="autoplay"></iframe>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Page
