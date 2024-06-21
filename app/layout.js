247b4d79bba5df6e863e3cc9b9a2ff0efa9beaa7
zbytes2227 <zbytes0@gmail.com>
2024-06-21 15:13:34 +0530

    final touch

--- app/layout.js
+++ app/layout.js
@@ -13,12 +13,34 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
+        <Navbar />
         <div className="md:pl-24 md:ml-40">
-        {children}
+          {children}
         </div>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js" defer  />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js" defer />
+
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+      !function(t,e){
+        t.artibotApi={l:[],t:[],on:function(){this.l.push(arguments)},trigger:function(){this.t.push(arguments)}};
+        var a=!1,i=e.createElement("script");
+        i.async=!0,i.type="text/javascript",i.src="https://app.artibot.ai/loader.js";
+        e.getElementsByTagName("head").item(0).appendChild(i);
+        i.onreadystatechange=i.onload=function(){
+          if(!(a||this.readyState&&"loaded"!=this.readyState&&"complete"!=this.readyState)){
+            new window.ArtiBot({i:"d569fab1-754b-4a88-9488-a2826f99ad59"});
+            a=!0
+          }
+        }
+      }(window,document);
+    `,
+          }}
+        />
+
       </body>
+
     </html>
   );
 }
