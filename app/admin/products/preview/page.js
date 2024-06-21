56b6296e5af23006d40d475284c8258e80cd3f35
zbytes2227 <zbytes0@gmail.com>
2024-06-21 13:33:41 +0530

    pdf do

--- app/admin/products/preview/page.js
+++ app/admin/products/preview/page.js
@@ -10,6 +10,25 @@ const Page = () => {
     const search = searchParams.get('id')
     const [PDFid, setPDFid] = useState(search);
 
+
+
+
+    const handleShare = async () => {
+        if (navigator.share) {
+          try {
+            await navigator.share({
+              title: 'Share this link',
+              text: text,
+              url: url,
+            });
+            console.log('Content shared successfully');
+          } catch (error) {
+            console.error('Error sharing:', error);
+          }
+        } else {
+          console.log('Share API not supported in this browser');
+        }
+      };
     return (
         <>
             <div className='mt-20'>
@@ -30,12 +49,12 @@ const Page = () => {
     </svg>
     Download
   </a>
-  <button type="button" class="inline-flex items-center px-4 py-2 text-sm font-medium text-blue-900 bg-transparent border border-blue-900 rounded-e-lg hover:bg-blue-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-blue-500 focus:bg-blue-900 focus:text-white ">
+  <a href={`https://api.whatsapp.com/send?text=Hey👋, Check this PDF on Code Crafters: https://dashboard.fgpcodecrafters.site/admin/products/preview?id=${PDFid}`} type="button" class="inline-flex items-center px-4 py-2 text-sm font-medium text-blue-900 bg-transparent border border-blue-900 rounded-e-lg hover:bg-blue-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-blue-500 focus:bg-blue-900 focus:text-white ">
   <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
       <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
     </svg>
     Share
-  </button>
+  </a>
 </div>
 
                 {/* <a >Back</a>
